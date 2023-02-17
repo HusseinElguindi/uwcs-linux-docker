c6cdba72a025046a3aab633e9fecc5a4504b2fa6
HusseinElguindi <34726859+HusseinElguindi@users.noreply.github.com>
2023-02-17 10:03:54 -0500

    add sync to remote script

--- Makefile
+++ Makefile
@@ -1,5 +1,2 @@
 build:
 	docker build -t "studentcs:1.0" .
-
-run:
-	docker run -it --rm -v $PWD:/tmp -w /tmp studentcs:1.0
---
+++ sync_up.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Copies the current directory as an archive to the remote server
+
+WATUSER="yourusername" # uWaterloo username
+REMOTEDIR="~/cs138/a3" # Any remote directory to copy to
+
+# Extra rsync options (--delete removes files on the remote that are not in your local copy)
+# OPTS="--delete --exclude=.ccls-cache" # uncomment for --delete (ensure that your REMOTEDIR is correct before doing so)
+OPTS="--exclude=.ccls-cache"
+
+HOST="linux.student.cs.uwaterloo.ca"
+rsync -av --progress $OPTS --exclude=.git --exclude="tmp.*" --exclude="scripts/outputs-actual" -e ssh ./ $WATUSER@$HOST:$REMOTEDIR
